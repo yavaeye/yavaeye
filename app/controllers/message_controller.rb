000ec180eb3f6b6ee3f356c763b62fc60b8ef41f
Saito <saitowu@gmail.com>
2011-10-06 03:41:18 +0800

    add messages impl

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,8 +1,26 @@
-put '/messages/:id/read' do
-
+put '/messages/:id/edit' do
+  message = Message.where(_id: id).first
+  if message
+    status 404
+  else
+    if message.update_attribute(params[:message])
+      status 200
+    else
+      status 500
+    end
+  end
 end
 
 delete '/messages/:id' do
-
+  message = Message.where(_id: id).first
+  if message
+    status 404
+  else
+    if message.delete
+      status 200
+    else
+      status 500
+    end
+  end
 end
 
