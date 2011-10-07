1cff82459e83de6fe8cc7467966a58bc1313ea5b
Saito <saitowu@gmail.com>
2011-10-07 19:58:59 +0800

    fix hottest

--- test/unit/hot_test.rb
+++ test/unit/hot_test.rb
@@ -5,6 +5,13 @@ class HotTest < TestCase
   def test_hot_value
     assert_equal true, Hot.value(1, Time.now) > Hot.value(1, Time.new(2011,7,3))
     assert_equal true, Hot.value(10, Time.now) > Hot.value(1, Time.now)
+    100.times.each do |i|
+      p Hot.value(i, Time.now)
+    end
+    p '========================================='
+    100.times.each do |i|
+      p Hot.value(i, Time.now.to_i + i * 60)
+    end
   end
 end
 
