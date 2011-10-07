9d427c8eb165adca40a7b89e90fc584a7ee69f64
Saito <saitowu@gmail.com>
2011-10-08 02:28:04 +0800

    fix hot algo

--- lib/hot.rb
+++ lib/hot.rb
@@ -5,8 +5,8 @@ module Hot
     time_seed = Time.new(2011,3,28,22,00,16,"+08:00")
     order = Math.log10([score.abs, 1].max)
     sign = score <=> 0
-    seconds = (time.to_i - time_seed.to_i + sign * order * 10800)
-    order + seconds * 1.0 / 45000
+    seconds = (time.to_i - time_seed.to_i)
+    sign * order + seconds * 1.0 / 45000
   end
 end
 
