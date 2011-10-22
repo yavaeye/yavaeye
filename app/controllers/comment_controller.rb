c8ed31f6e5bc5a8fe6ee418b8c36de4cb0f5aa3d
Saito <saitowu@gmail.com>
2011-10-22 23:16:17 +0800

    comment post and delete support

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,23 +1,32 @@
-get '/comments/:id' do |id|
+##encoding: utf-8
+get '/comment/:id' do |id|
   comment = Comment.where(_id: id).first
   respond_with :'comment/show', comment
 end
 
-get '/comments/new' do
+get '/comment/new' do
   respond_with :'comment/new', Comment.new
 end
 
-post '/comments' do
-  post = Post.where(_id: params[:post_id]).first
-  comment = Comment.create(params[:comment], user: current_user, post: post)
-  if comment.persisted?
-      status 200
+post '/comment' do
+  authenticate!
+  @post = Post.where(_id: params[:post_id]).first
+  @comment = Comment.new(params[:comment])
+  @comment.user = current_user
+  @comment.post = @post
+  @comment.save
+  if @comment.persisted?
+    respond_to do |f|
+      f.html { flash[:notice] = '吐槽成功'; redirect "/post/#{@comment.post.token}" }
+      f.json { @comment.to_json }
+    end
   else
-      respond_with :'comment/new', comment
+    respond_with :"/post/#{@comment.post.token}", @comment.errors
   end
 end
 
-put '/comments/:id' do |id|
+put '/comment/:id' do |id|
+  authenticate!
   comment = Comment.where(_id! id).first
   if comment.blank?
     status 404
@@ -30,16 +39,10 @@ put '/comments/:id' do |id|
   end
 end
 
-delete '/comments/:id' do |id|
-  comment = Comment.where(_id! id).first
-  if comment.blank?
-    status 404
-  else
-    if comment.delete
-      status 200
-    else
-      respond_with :'comment/:id/edit', comment
-    end
-  end
+delete '/comment/:id' do |id|
+  authenticate!
+  @comment = Comment.where(_id: id, user_id: session[:user_id]).first
+  flash[:notice] = @comment.destroy ? '删掉了' : '删不掉'
+  redirect "/post/#{@comment.post.token}"
 end
 
