621316388ca1e0e6f0e186eae932dd6badf57df0
NS <ns@theplant.jp>
2011-12-03 22:02:40 +0800

    test on boot_timestamp

--- test/unit/yava_utils_test.rb
+++ test/unit/yava_utils_test.rb
@@ -1,10 +1,14 @@
 require_relative '../test_helper'
 
 class YavaUtilsTest < TestCase
+  include YavaUtils
 
   def test_hot_value
-    assert_equal true, YavaUtils.hot_value(1, Time.now) > YavaUtils.hot_value(1, Time.new(2011,7,3))
-    assert_equal true, YavaUtils.hot_value(10, Time.now) > YavaUtils.hot_value(1, Time.now)
+    assert_equal true, hot_value(1, Time.now) > hot_value(1, Time.new(2011,7,3))
+    assert_equal true, hot_value(10, Time.now) > hot_value(1, Time.now)
+  end
+
+  def test_boot_timestamp
+    assert boot_timestamp <= Time.now.to_i
   end
 end
-
