8f1641076f68ebdf68880821395ca05d444470bc
Saito <saitowu@gmail.com>
2011-12-03 23:42:50 +0800

    fix board post issue

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -28,14 +28,13 @@ post '/board' do
   authenticate!
   @board = Board.new params[:board]
   @board.user = current_user
-  @board.save
-  if @board.persisted?
+  if @board.save
     respond_to do |f|
       f.html { flash[:notice] = '提交申请,请静候领导审批.'; redirect '/' }
       f.json { @board.to_json }
     end
   else
-    respond_with :'post/index', @board
+    respond_with :'board/new', @board
   end
 end
 
