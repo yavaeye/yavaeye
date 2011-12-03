3ae8aa8051067248e68f93d8a15c59bb00fe5e66
Saito <saitowu@gmail.com>
2011-12-03 18:10:37 +0800

    add post factory

--- test/factory/factories.rb
+++ test/factory/factories.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 Factory.define :user do |u|
   u.sequence(:openid){ |i| "http://me.yahoo.com/mm62xOv9#{i}" }
   u.sequence(:nick){ |i| "yavaeye#{i}" }
@@ -11,3 +13,9 @@ Factory.define :board do |b|
   b.user { User.first or Factory(:user) }
 end
 
+Factory.define :post do |p|
+  p.title 'yavaeye.com'
+  p.link 'http://yavaeye.com'
+  p.user { User.first or Factory(:user) }
+  p.board { Board.first or Factory(:board) }
+end
