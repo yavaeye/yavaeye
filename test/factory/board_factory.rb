a9c8fc46ba86e6692e140f1e5c2556cd3ea3437d
Saito <saitowu@gmail.com>
2011-12-03 16:22:53 +0800

    refactory factory to one file

--- test/factory/board_factory.rb
+++
@@ -1,7 +0,0 @@
-Factory.define :board do |b|
-  b.name 'random'
-  b.description 'random board'
-  b.active true
-  b.user { User.first or Factory(:user) }
-end
-
--- test/factory/factories.rb
+++ test/factory/factories.rb
@@ -3,3 +3,11 @@ Factory.define :user do |u|
   u.sequence(:nick){ |i| "yavaeye#{i}" }
   u.sequence(:email){ |i| "yavaeye#{i}@gmail.com" }
 end
+
+Factory.define :board do |b|
+  b.name 'random'
+  b.description 'random board'
+  b.active true
+  b.user { User.first or Factory(:user) }
+end
+
