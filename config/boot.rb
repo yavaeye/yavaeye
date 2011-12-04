af2644a998c4c4945899ac148534bce1674d0b47
NS <ns@theplant.jp>
2011-12-04 16:09:09 +0800

    replace global variable with configure entry

--- config/boot.rb
+++ config/boot.rb
@@ -30,15 +30,18 @@ use Rack::YavaProtection
 
 configure :development do
   also_reload "lib/**/*.rb", "app/{models,helpers}/*.rb"
-  require "./script/asset"
+
   # proxy for openid
   if port = ENV['openid_proxy_port']
     puts "=> Using proxy for openid request"
     proxy = Net::HTTP.SOCKSProxy '127.0.0.1', port.to_i
     OpenID.fetcher.instance_variable_set :@proxy, proxy
   end
-  $asset = Asset.new
-  before { I18n.locale = :'zh-CN'; $asset.compile }
+
+  # js/css autocompile
+  require "./script/asset"
+  set :asset, Asset.new
+  before { I18n.locale = :'zh-CN'; settings.asset.compile }
 end
 
 puts "=> Loading I18n"
