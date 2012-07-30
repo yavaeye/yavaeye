c58a9d324ba643ac24b64b1130c71ae366454275
NS <ns@theplant.jp>
2012-07-30 16:13:22 +0800

    init migrations

--- migrate/1343578454_init_tables.rb
+++ migrate/1343578454_init_tables.rb
@@ -4,5 +4,65 @@ class InitTables < ActiveRecord::Migration
       t.string :keys
       t.hstore :values
     end
+
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :intro
+      t.string :gravatar_id
+      t.float :karma, default: 0.0
+
+      t.timestamps
+    end
+
+    create_table :profiles do |t|
+      t.integer :user_id
+      t.column :marked_post_ids, 'integer[]'
+      t.column :read_post_ids, 'integer[]'
+      t.column :mentions, 'hstore' # type: text
+      t.column :tag_category, 'hstore'
+
+      t.timestamps
+    end
+
+    create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :link
+      t.string :domain
+      t.float :score
+      t.text :content
+      t.text :content_html
+
+      t.column :tags, 'varchar(255)[]'
+      t.column :keywords, 'tsvector'
+      t.column :liker_ids, 'integer[]'
+
+      t.date :deleted_at
+      t.timestamps
+    end
+
+    create_table :users_liked_posts, id: false do |t|
+      t.integer :user_id
+      t.integer :post_id
+    end
+
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.text :content
+      t.text :content_html
+
+      t.date :deleted_at
+      t.timestamps
+    end
+
+    create_table :tags do |t|
+      t.string :name
+      t.text :content
+      t.text :content_html
+
+      t.timestamps
+    end
   end
 end
---
+++ migrate/1343635491_add_indices.rb
@@ -0,0 +1,13 @@
+class AddIndices < ActiveRecord::Migration
+  def change
+    add_index :users, :name
+    add_index :users, :email
+    add_index :profiles, :user_id
+    add_index :posts, :user_id
+    add_index :users_liked_posts, :user_id
+    add_index :users_liked_posts, :post_id
+    add_index :comments, :user_id
+    add_index :comments, :post_id
+    add_index :tags, :name
+  end
+end
